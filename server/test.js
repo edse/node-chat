8fde3005c61cfcacffba51c0358e14510f1d4284
Emerson Estrella <emersonestrella@edse.local>
2013-09-24 18:35:23 -0300

    test

--- server/test.js
+++ server/test.js
@@ -17,8 +17,8 @@ var userSchema = mongoose.Schema({
 
 // assign a function
 userSchema.methods.messages = function (cb) {
-  return this.model('Message').find({ user: this.type }, cb);
-}
+  return Message.find({ user: this._id }, cb);
+};
 
 // room schema
 var roomSchema = mongoose.Schema({
@@ -31,22 +31,34 @@ var roomSchema = mongoose.Schema({
 var messageSchema = mongoose.Schema({
   date: { type: Date, default: Date.now },
   user: { type: mongoose.Schema.ObjectId, ref: 'User', index: true },
-  room: { type: mongoose.Schema.ObjectId, ref: 'User', index: true },
+  room: { type: mongoose.Schema.ObjectId, ref: 'Room', index: true },
   message: String,
   available: { type: Boolean, default: true }
 });
-// ensureIndex to false
-messageSchema.set('autoIndex', false);
+// ensureIndex to false in production
+// messageSchema.set('autoIndex', false);
 
 // Model
 var User = mongoose.model('User', userSchema);
 var Room = mongoose.model('Room', roomSchema);
 var Message = mongoose.model('Message', messageSchema);
 
-// Document
-var user_1 = new User({ name: 'Emerson Estrella', available: true })
-console.log(user_1.name) // 'Silence'
+// User Document
+var user_1 = new User({ name: 'Emerson Estrella', available: true });
 
-console.log('1');
+user_1.save(function (err, user_1) {
+  if (err){ console.log(err); }
+  console.log("User "+user_1.name+" saved!");
+  
+  // Message Document
+  var msg_1 = new Message({ date: Date.now(), user: user_1._id, message: "Test 1 2 3...", available: true });
+  msg_1.save(function (err, msg_1){
+    if (err){ console.log(err); }
+    console.log("Message "+msg_1.message+" saved!");
 
+    console.log(user_1.name);
+    console.log(user_1.messages());
 
+  });
+
+});
